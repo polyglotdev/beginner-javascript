aa3851ebf9d260fddcd5cfcad0c1929137ccee2b
codefuldom <dom@codefuldom.com>
2020-04-22 09:16:06 -0500

    :rocket: add string example with interpolation and concatenation

--- my-work/strings/strings.js
+++ my-work/strings/strings.js
@@ -1,2 +1,13 @@
 console.log('this is the remix')
 console.log(`1 + 1 = ${1 + 1}`)
+const song = `
+Oh yeah I like pizza
+But sometimes I like pie.
+`
+console.log(song)
+
+const name = 'Dom'
+const sentence = 'Hello my name is '
+const concatSentenceAndName = `${sentence} ${name}!`
+
+console.log(concatSentenceAndName)
