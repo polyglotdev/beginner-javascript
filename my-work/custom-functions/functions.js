ede10596def85bccda21adb8b11a9b9c89eab61a
codefuldom <dom@codefuldom.com>
2020-04-22 10:49:22 -0500

    :rocket: use parameters and args

--- my-work/custom-functions/functions.js
+++ my-work/custom-functions/functions.js
@@ -2,7 +2,20 @@ function calculateBill(tip) {
   const billBeforeTip = 100
   const salesTax = 1.13
   const totalBill = billBeforeTip + tip + salesTax
-  return totalBill.toFixed(2)
+  return parseFloat(totalBill.toFixed(2))
 }
 
 calculateBill(10) /* ? */
+console.log(`Your total bill is: ${calculateBill(10)}`)
+
+// First Class Citizens
+const math = (num1, num2) => num1 + num2
+const add = () => math(2, 2)
+
+add() /* ? */
+
+function doctor(firstName) {
+  return `Dr. ${firstName}`
+}
+
+doctor('Dom Hallan') /* ? */
