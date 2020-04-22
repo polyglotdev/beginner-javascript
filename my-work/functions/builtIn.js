e1b7ede49dc10d5bf7da3d10307e55e5f37fd3ea
codefuldom <dom@codefuldom.com>
2020-04-22 10:16:56 -0500

    :rocket: add function to my-work directory

--- my-work/functions/builtIn.js
+++ my-work/functions/builtIn.js
@@ -4,3 +4,12 @@ const max = Math.max(10, 12)
 console.log(max)
 
 console.log(Math.floor(2.4444))
+
+console.log(parseFloat('202'))
+
+console.log(Date.now())
+
+console.log(new Date())
+
+const p = document.querySelector('p')
+console.log(p)
