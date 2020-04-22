1135ced5ccaa5efeda254ffe1b37c0bbb304729d
codefuldom <dom@codefuldom.com>
2020-04-22 07:30:11 -0500

    :rocket: add variable.html and variable.js

---
+++ my-work/variables/variables.js
@@ -0,0 +1,12 @@
+// function or potentially globally scoped
+var first = 'elijah'
+
+// block scoped
+let age = 300
+const myName = 'ezra'
+
+console.log(`first: ${first}`)
+console.log(`age: ${age}`)
+console.log(`my name is ${myName}`)
+
+// Scope: Where are my variables available to me?
