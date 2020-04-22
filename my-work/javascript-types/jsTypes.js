225652b59190c69351dbf76d10da1ed823a203cd
codefuldom <dom@codefuldom.com>
2020-04-22 08:49:06 -0500

    :rocket: add javascript types

---
+++ my-work/javascript-types/jsTypes.js
@@ -0,0 +1,23 @@
+// String
+const name = 'Dominique'
+
+// Number
+const myAge = 37
+
+// Object
+const person = {
+  name: 'Dom',
+  age: 37,
+}
+
+// Boolean
+const likesMilk = false
+
+// Null
+const likeSTL = null
+
+// Undefined
+console.log(flavor)
+
+// Symbol
+const mySymbol = Symbol('this is my symbol')
