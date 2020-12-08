922e299c16ab5f9c33869ff846a25eb1fbca8026
Dom Hallan <dom@polyglotdev.com>
2020-12-08 16:05:07 -0600

    complete the variable section

---
+++ polyglotdev-work/module-1/strings/strings.js
@@ -0,0 +1,23 @@
+/*
+SNOB'N'US
+
+String
+Number
+Object
+Boolean
+Null
+Undefined
+Symbol
+*/
+
+let firstName = 'Ezra'
+let lastName = 'Hallan'
+console.log(`${firstName} ${lastName}`)
+
+let status = true
+let sentence = `She's so cool.`
+console.log(sentence)
+console.log(status)
+
+let hello = `Hello, ${firstName}!`
+console.log(hello)
