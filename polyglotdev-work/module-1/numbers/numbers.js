5134c0eeb69cdc3aa00fdc671535ee8712abc55d
Dom Hallan <dom@polyglotdev.com>
2020-12-28 20:50:51 -0600

    🎆 complete section object

---
+++ polyglotdev-work/module-1/numbers/numbers.js
@@ -0,0 +1,11 @@
+const age = 37
+let salary = 269
+console.log(`I am ${age} years old and make ${salary},000 per year`)
+
+console.log(typeof age)
+console.log(typeof salary)
+
+const smarties = 20
+const numberOfKids = 3
+const eachKidGets = Math.floor(smarties / numberOfKids)
+console.log(`Each kid gets ${eachKidGets}`)
---
+++ polyglotdev-work/module-1/objects/objects.js
@@ -0,0 +1,6 @@
+// Objects
+const person = {
+  name: `Dom Hallan`,
+  age: 36,
+  occupation: `software engineer`
+}
